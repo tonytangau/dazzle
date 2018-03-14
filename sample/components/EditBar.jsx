fa16b1aec845d368950f4573bafc810456160de2
Tony Tang <tony.cn@outlook.com>
2018-03-14 16:18:42 +0800

    Update sample styles

--- sample/components/EditBar.jsx
+++ sample/components/EditBar.jsx
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types';
 const EditBar = ({ onEdit }) => (
   <div className="row edit-bar">
     <div className="col-sm-12 text-right">
-      <button type="button" className="btn btn-default btn-xs" onClick={onEdit}>
+      <button type="button" className="btn btn-default" onClick={onEdit}>
         <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-          Edit
-        </button>
+        <span className="edit-text">Edit</span>
+      </button>
     </div>
   </div>
 );
